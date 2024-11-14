33cd8d791f09431288c2e65066f36e1921e24c05
Sabyasachi Mohanty <sabyasachim999@gmail.com>
2024-11-14 22:22:36 +0530

    Added item, sounds, items_collection logic and completed project

--- script.js
+++ script.js
@@ -6,9 +6,37 @@ let pressedLeft = false
 let pressedRight = false
 canvas.height = HEIGHT
 canvas.width = WIDTH
+const key_img = new Image()
+key_img.src = "./assets/key.png"
+const coin_img = new Image()
+coin_img.src = "./assets/coin.png"
+const door_img = new Image()
+door_img.src = "./assets/door.png"
+let coinCollected = false
+let keyCollected = false
+const jump_sound = new Audio("./assets/audio/jump.mp3")
+const fall_sound = new Audio("./assets/audio/fall.mp3")
+const coinPickup_sound = new Audio("./assets/audio/coin_pickup.mp3")
+const win_sound = new Audio("./assets/audio/win.mp3")
+
+
 const ctx = canvas.getContext("2d")
 ctx.fillStyle = "black"
 ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+class Item {
+    constructor(src, x, y, width, height) {
+        this.src = src
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+
+    render() {
+        ctx.drawImage(this.src, this.x, this.y, this.width, this.height)
+    }
+}
 
 class Platform {
     constructor(x, y, width) {
@@ -65,7 +93,7 @@ function renderPlatforms() {
 class Player {
     constructor() {
         this.x = 50
-        this.y = 50
+        this.y = 375
         this.height = 50
         this.width = 50
         this.dy = 0
@@ -98,9 +126,10 @@ class Player {
     collide(platform) {
         if (
             this.y + this.height >= platform.y &&
-            this.y <= platform.y &&
+            this.y < platform.y &&
             this.x + this.width >= platform.x &&
-            this.x <= platform.x + platform.width
+            this.x <= platform.x + platform.width && 
+            this.dy >= 0
         ) {
             return true
         }
@@ -110,6 +139,29 @@ class Player {
 
 const player = new Player()
 player.render()
+const coin = new Item(coin_img, 130, 90, 50, 50)
+const key = new Item(key_img, 650, 50, 25, 50)
+const door = new Item(door_img, 600, 325, 100, 100)
+
+function collison(object) {
+    if (
+        player.x + player.width < object.x ||
+        player.x > object.x + object.width ||
+        player.y + player.height < object.y ||
+        player.y > object.y + object.height
+    ) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function restart() {
+    player.x = 50
+    player.y = 375
+    coinCollected = false
+    keyCollected = false
+}
 
 function gameloop() {
     if (pressedLeft) {
@@ -119,9 +171,31 @@ function gameloop() {
     } else {
         player.dx = 0
     }
+
+    if (collison(coin)) {
+        coinCollected = true
+        coinPickup_sound.play()
+    }
+    if (collison(key) && coinCollected) {
+        keyCollected = true
+        coinPickup_sound.play()
+    }
+    if (collison(door) && keyCollected) {
+        win_sound.play()
+        window.alert("You won")
+        restart()
+    }
+    if (player.y >= canvas.height) {
+        fall_sound.play()
+        restart()
+    }
+
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     renderPlatforms()
+    if (!coinCollected) coin.render()
+    if (!keyCollected) key.render()
+    door.render()
     player.update()
     requestAnimationFrame(gameloop)
 }
@@ -140,6 +214,7 @@ window.addEventListener("keydown", function(e) {
             if (!player.isJumping) {
                 player.dy = -15
                 player.isJumping = true
+                jump_sound.play()
             }
             break
     }
